694d016915ca840e12b1096ddbd2a998b2754d06
Aaron Addleman <aaronaddleman@gmail.com>
2017-02-28 21:09:52 -0800

    added testing of values for beginWord and endWord

--- test_words.py
+++ test_words.py
@@ -2,9 +2,14 @@ from words import wordLadder
 from unittest import TestCase
 
 class wordLadderTest(TestCase):
-  def test_word_accepted(self):
+  def test_beingWord_accepted(self):
     wl = wordLadder("hit", "cog", ["hot", "dot", "dog", "log", "cog"])
     wl.beginWord = "hit"
+    self.assertEqual("hit", wl.beginWord)
+  def test_endWord_accepted(self):
+    wl = wordLadder("hit", "cog", ["hot", "dot", "dog", "log", "cog"])
+    wl.endWord = "cog"
+    self.assertEqual("cog", wl.endWord)
   def test_word_length(self):
     wl = wordLadder("hit", "cog", ["hot", "dot", "dog", "log", "cog"])
     self.assertTrue(wl.wordLength("hit"))
