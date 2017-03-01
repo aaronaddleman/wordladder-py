9592f576c3806dfbd822b4fd14ff275600cc6d0c
Aaron Addleman <aaronaddleman@gmail.com>
2017-02-28 21:05:57 -0800

    added test of word length

--- test_words.py
+++ test_words.py
@@ -3,8 +3,9 @@ from unittest import TestCase
 
 class wordLadderTest(TestCase):
   def test_word_accepted(self):
-    wl = wordLadder()
+    wl = wordLadder("hit", "cog", ["hot", "dot", "dog", "log", "cog"])
     wl.beginWord = "hit"
   def test_word_length(self):
-    wl = wordLadder()
-    wl.wordLength("hit")
+    wl = wordLadder("hit", "cog", ["hot", "dot", "dog", "log", "cog"])
+    self.assertTrue(wl.wordLength("hit"))
+    self.assertFalse(wl.wordLength("wordtoolong"))
